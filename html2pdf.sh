e6f313414843a1b8191a53ab16c37147b46ebcad
Chaim Halbert <chaim.leib.halbert@gmail.com>
2024-03-18 13:09:40 -0700

    html2pdf: replace --print-to-pdf-no-header with --no-pdf-header-footer, error out if no chrome, allow CLI brave-browser

--- html2pdf.sh
+++ html2pdf.sh
@@ -4,18 +4,23 @@ CHROME=
 for prog in \
   /Applications/"Brave Browser.app"/Contents/MacOS/"Brave Browser" \
   /Applications/"Chromium.app"/Contents/MacOS/"Chromium" \
-  chromium \
-  chrome \
+  "$(command -v brave-browser)" \
+  "$(command -v chromium)" \
+  "$(command -v chrome)" \
   ; do
   if [ -x "$prog" ]; then
     CHROME=$prog
     break
   fi
 done
+if [[ -z "$CHROME" ]]; then
+  echo "no chrome found" >&2
+  exit 1
+fi
 echo "found chrome: $CHROME"
 exec "$CHROME" \
   --headless \
   --disable-gpu \
-  --print-to-pdf-no-header \
+  --no-pdf-header-footer \
   --print-to-pdf="$2" \
   "$1"
